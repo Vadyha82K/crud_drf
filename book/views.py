a5d2db368513ed2aae21731e2a80e3411f1ad783
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-26 11:32:08 +0300

    Представление на основе класса GenericAPIView классов миксинов

--- book/views.py
+++ book/views.py
@@ -1,83 +1,38 @@
 from django.http import Http404
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+    DestroyModelMixin
 from rest_framework.response import Response
-from rest_framework.views import APIView
+
 
 from book.models import Book
 from book.serializers import BookSerializer
 
 
-class BookListView(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+class BookListView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def post(self, request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class BookDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            raise Http404
+class BookDetailView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
 
-    def get(self, request, pk):
-        book = self.get_object(pk)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        book = self.get_object(pk)
-        serializer = BookSerializer(book, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-    
-    def delete(self, request, pk):
-        book = self.get_object(pk)
-        book.delete()
-        return Response(status=204)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
 
-# @api_view(['GET', 'POST'])
-# def books_list(request):
-#     if request.method == 'GET':
-#         items = Book.objects.all()
-#         serializer = BookSerializer(items, many=True)
-#         return Response(serializer.data)
-#     if request.method == 'POST':
-#         serializer = BookSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=201)
-#         return Response(serializer.errors, status=400)
-#
-#
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def book_detail(request, pk):
-#     try:
-#         book = Book.objects.get(pk=pk)
-#     except Book.DoesNotExist:
-#         return Response(status=404)
-#
-#     if request.method == 'GET':
-#         serializer = BookSerializer(book)
-#         return Response(serializer.data)
-#
-#     elif request.method == 'PUT':
-#         serializer = BookSerializer(book, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=400)
-#
-#     elif request.method == 'DELETE':
-#         book.delete()
-#         return Response(status=204)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
